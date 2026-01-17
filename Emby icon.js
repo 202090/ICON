648ff46cdb31841bde25b5ea5680104446ec7802
202090 <55238999+202090@users.noreply.github.com>
2026-01-18 00:36:31 +0800

    更新 Emby icon.js

--- Emby icon.js
+++ Emby icon.js
@@ -1,1 +1,9 @@
-
+{
+"name":"Emby Icon", 
+"description":"自用图标集", 
+"icons":
+    [
+	{"name":"Meowfly", "url":"https://raw.githubusercontent.com/202090/ICON/refs/heads/main/Emby%20icon/Meowfly.png"},
+    {"name":"MOOGUU_Ultra", "url":"https://raw.githubusercontent.com/202090/ICON/refs/heads/main/Emby%20icon/MOOGUU-Ultra.png"}
+    ]
+}
